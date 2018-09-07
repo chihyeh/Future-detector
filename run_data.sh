71e2a24b2ab4be33928ad690bcc06c638fda411c
Chih-Hsiang Yeh <a9510130375@gmail.com>
2018-09-07 08:33:44 +0800

    New

--- run_data.sh
+++ run_data.sh
@@ -1,25 +1,25 @@
-ls /eos/cms/store/group/dpg_hgcal/tb_hgcal/2018/cern_h2_june/offline_analysis/sim_ntuples/v9/ntuple_24.root > input.txt
+ls /eos/cms/store/group/dpg_hgcal/tb_hgcal/2018/cern_h2_june/offline_analysis/ntuple/v9/ntuple_24.root > input.txt
 ./makePlots 1
-ls /eos/cms/store/group/dpg_hgcal/tb_hgcal/2018/cern_h2_june/offline_analysis/sim_ntuples/v9/ntuple_25.root > input.txt
+ls /eos/cms/store/group/dpg_hgcal/tb_hgcal/2018/cern_h2_june/offline_analysis/ntuple/v9/ntuple_25.root > input.txt
 ./makePlots 1
-ls /eos/cms/store/group/dpg_hgcal/tb_hgcal/2018/cern_h2_june/offline_analysis/sim_ntuples/v9/ntuple_26.root > input.txt
+ls /eos/cms/store/group/dpg_hgcal/tb_hgcal/2018/cern_h2_june/offline_analysis/ntuple/v9/ntuple_26.root > input.txt
 ./makePlots 1
-ls /eos/cms/store/group/dpg_hgcal/tb_hgcal/2018/cern_h2_june/offline_analysis/sim_ntuples/v9/ntuple_177.root > input.txt
+ls /eos/cms/store/group/dpg_hgcal/tb_hgcal/2018/cern_h2_june/offline_analysis/ntuple/v9/ntuple_177.root > input.txt
 ./makePlots 1
-ls /eos/cms/store/group/dpg_hgcal/tb_hgcal/2018/cern_h2_june/offline_analysis/sim_ntuples/v9/ntuple_178.root > input.txt
+ls /eos/cms/store/group/dpg_hgcal/tb_hgcal/2018/cern_h2_june/offline_analysis/ntuple/v9/ntuple_178.root > input.txt
 ./makePlots 1
-ls /eos/cms/store/group/dpg_hgcal/tb_hgcal/2018/cern_h2_june/offline_analysis/sim_ntuples/v9/ntuple_179.root > input.txt
+ls /eos/cms/store/group/dpg_hgcal/tb_hgcal/2018/cern_h2_june/offline_analysis/ntuple/v9/ntuple_179.root > input.txt
 ./makePlots 1
-ls /eos/cms/store/group/dpg_hgcal/tb_hgcal/2018/cern_h2_june/offline_analysis/sim_ntuples/v9/ntuple_365.root > input.txt
+ls /eos/cms/store/group/dpg_hgcal/tb_hgcal/2018/cern_h2_june/offline_analysis/ntuple/v9/ntuple_365.root > input.txt
 ./makePlots 1
-ls /eos/cms/store/group/dpg_hgcal/tb_hgcal/2018/cern_h2_june/offline_analysis/sim_ntuples/v9/ntuple_366.root > input.txt
+ls /eos/cms/store/group/dpg_hgcal/tb_hgcal/2018/cern_h2_june/offline_analysis/ntuple/v9/ntuple_366.root > input.txt
 ./makePlots 1
-ls /eos/cms/store/group/dpg_hgcal/tb_hgcal/2018/cern_h2_june/offline_analysis/sim_ntuples/v9/ntuple_367.root > input.txt
+ls /eos/cms/store/group/dpg_hgcal/tb_hgcal/2018/cern_h2_june/offline_analysis/ntuple/v9/ntuple_367.root > input.txt
 ./makePlots 1
-ls /eos/cms/store/group/dpg_hgcal/tb_hgcal/2018/cern_h2_june/offline_analysis/sim_ntuples/v9/ntuple_407.root > input.txt
+ls /eos/cms/store/group/dpg_hgcal/tb_hgcal/2018/cern_h2_june/offline_analysis/ntuple/v9/ntuple_407.root > input.txt
 ./makePlots 1
-ls /eos/cms/store/group/dpg_hgcal/tb_hgcal/2018/cern_h2_june/offline_analysis/sim_ntuples/v9/ntuple_409.root > input.txt
+ls /eos/cms/store/group/dpg_hgcal/tb_hgcal/2018/cern_h2_june/offline_analysis/ntuple/v9/ntuple_409.root > input.txt
 ./makePlots 1
-ls /eos/cms/store/group/dpg_hgcal/tb_hgcal/2018/cern_h2_june/offline_analysis/sim_ntuples/v9/ntuple_411.root > input.txt
+ls /eos/cms/store/group/dpg_hgcal/tb_hgcal/2018/cern_h2_june/offline_analysis/ntuple/v9/ntuple_411.root > input.txt
 ./makePlots 1
 

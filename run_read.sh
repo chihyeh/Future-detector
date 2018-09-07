d1e7ab56be6ff9ad8c73a0150c3ba8362d51743a
Chih-Hsiang Yeh <a9510130375@gmail.com>
2018-09-07 09:43:37 +0800

    New

---
+++ run_read.sh
@@ -0,0 +1,25 @@
+ls output_root/ntuple_24.root > input.txt
+./makePlots
+ls output_root/ntuple_25.root > input.txt
+./makePlots
+ls output_root/ntuple_26.root > input.txt
+./makePlots
+ls output_root/ntuple_177.root > input.txt
+./makePlots
+ls output_root/ntuple_178.root > input.txt
+./makePlots
+ls output_root/ntuple_179.root > input.txt
+./makePlots
+ls output_root/ntuple_365.root > input.txt
+./makePlots
+ls output_root/ntuple_366.root > input.txt
+./makePlots
+ls output_root/ntuple_367.root > input.txt
+./makePlots
+ls output_root/ntuple_407.root > input.txt
+./makePlots
+ls output_root/ntuple_409.root > input.txt
+./makePlots
+ls output_root/ntuple_411.root > input.txt
+./makePlots 
+

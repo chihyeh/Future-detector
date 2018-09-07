3d3b3881a15310758b3c31c1acbb603f3608e1f6
Chih-Hsiang Yeh <a9510130375@gmail.com>
2018-09-07 09:48:47 +0800

    New

--- run_read.sh
+++ run_read.sh
@@ -1,25 +1,27 @@
-ls output_root/ntuple_24.root > input.txt
+ls output_root/Run177_50GeV_Ele.root  > input.txt
 ./makePlots
-ls output_root/ntuple_25.root > input.txt
+ls output_root/Run178_50GeV_Ele.root  > input.txt
 ./makePlots
-ls output_root/ntuple_26.root > input.txt
+ls output_root/Run24_100GeV_Ele.root  > input.txt
 ./makePlots
-ls output_root/ntuple_177.root > input.txt
+ls output_root/Run25_100GeV_Ele.root  > input.txt
 ./makePlots
-ls output_root/ntuple_178.root > input.txt
+ls output_root/Run26_100GeV_Ele.root  > input.txt
 ./makePlots
-ls output_root/ntuple_179.root > input.txt
+ls output_root/Run365_80GeV_Ele.root  > input.txt
 ./makePlots
-ls output_root/ntuple_365.root > input.txt
+ls output_root/Run366_80GeV_Ele.root  > input.txt
 ./makePlots
-ls output_root/ntuple_366.root > input.txt
+ls output_root/Run367_80GeV_Ele.root  > input.txt
 ./makePlots
-ls output_root/ntuple_367.root > input.txt
+ls output_root/Run407_100GeV_Ele.root  > input.txt
 ./makePlots
-ls output_root/ntuple_407.root > input.txt
+ls output_root/Run409_100GeV_Ele.root  > input.txt
 ./makePlots
-ls output_root/ntuple_409.root > input.txt
+ls output_root/Run416_100GeV_Ele.root  > input.txt
 ./makePlots
-ls output_root/ntuple_411.root > input.txt
-./makePlots 
+ls output_root/Run417_100GeV_Ele.root > input.txt
+./makePlots
+ls output_root/Run417_150GeV_Ele.root > input.txt
+./makePlots
 
